e3fb3a26878920ad05f35e44905b3fcface1d360
Roxanne Weber <marie.sankofa@gmail.com>
2022-02-11 08:52:52 -0600

    first middleware logger setup and working

--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const server = express();
 
+const actionsRouter = require('./actions/actions-router')
+const projectsRouter = require('./projects/projects-router')
+
+server.use(logger); 
+
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
+
+function logger(req, res, next) {
+	const timestamp = new Date().toLocaleString();
+	const method = req.method;
+	const url = req.originalUrl;
+	console.log(`${timestamp}, ${method} request to ${url}`);
+	next();
+}
